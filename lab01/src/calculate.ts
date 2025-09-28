05bde9bdba5f6b15ca752d18d3ca231fe1caf372
Gleb Gleb <g.estraikh@gmail.com>
2025-09-28 18:04:36 +0700

    add final version

--- lab01/src/calculate.ts
+++ lab01/src/calculate.ts
@@ -3,18 +3,42 @@ import grammar, { AddMulActionDict } from "./addmul.ohm-bundle";
 
 export const addMulSemantics: AddMulSemantics = grammar.createSemantics() as AddMulSemantics;
 
-
 const addMulCalc = {
-/// write the action rules here
+    AddExpr_add(left, _op, right) {
+        return left.calculate() + right.calculate();
+    },
+
+    AddExpr(expr) {
+        return expr.calculate();
+    },
+
+    MulExpr_mul(left, _op, right) {
+        return left.calculate() * right.calculate();
+    },
+
+    MulExpr(expr) {
+        return expr.calculate();
+    },
+
+    PrimExpr_paren(_open, expr, _close) {
+        return expr.calculate();
+    },
+
+    PrimExpr(expr) {
+        return expr.calculate();
+    },
+
+    number(digits) {
+        return parseInt(this.sourceString, 10);
+    }
 } satisfies AddMulActionDict<number>
 
-addMulSemantics.addOperation<Number>("calculate()", addMulCalc);
+addMulSemantics.addOperation<number>("calculate()", addMulCalc);
 
-interface AddMulDict  extends Dict {
+interface AddMulDict extends Dict {
     calculate(): number;
 }
 
-interface AddMulSemantics extends Semantics
-{
+interface AddMulSemantics extends Semantics {
     (match: MatchResult): AddMulDict;
 }
