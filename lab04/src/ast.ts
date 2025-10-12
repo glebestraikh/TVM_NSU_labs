f7dc4fe9e25e7dbef341d2a9f5dd313a12404083
Gleb Gleb <g.estraikh@gmail.com>
2025-10-12 21:23:40 +0700

    add first working version

--- lab04/src/ast.ts
+++ lab04/src/ast.ts
@@ -1,1 +1,32 @@
-export type Expr = {};
+export type Expr =
+    | NumConst
+    | Variable
+    | BinaryOp
+    | UnaryMinus;
+
+// Числовая константа
+export interface NumConst {
+    type: 'const';
+    value: number;
+}
+
+// Переменная
+export interface Variable {
+    type: 'var';
+    name: string;
+}
+
+// Бинарные операции: +, -, *, /
+export interface BinaryOp {
+    type: 'binop';
+    op: '+' | '-' | '*' | '/';
+    left: Expr;
+    right: Expr;
+}
+
+// Унарный минус
+export interface UnaryMinus {
+    type: 'unary';
+    op: '-';
+    argument: Expr;
+}
