2d85210165838c4fbf3a4911435dcbe2266f3346
Gleb Gleb <g.estraikh@gmail.com>
2025-10-20 11:26:41 +0700

    add working version

--- lab05/src/index.ts
+++ lab05/src/index.ts
@@ -7,19 +7,27 @@ export async function parseCompileAndExecute(expression: string, ...args: number
     let variables = getVariables(expr);
     return await compileAndExecute(expr, variables, ...args);
 }
-export async function compileAndExecute(expr: Expr, variables: string[], ...args: number[]): Promise<number>
-{
+export async function compileAndExecute(expr: Expr, variables: string[], ...args: number[]): Promise<number> {
     let wasmFunc = await compile(expr, variables);
     return wasmFunc(...args);
 }
 
 export const compile = async (expr: Expr, variables: string[]) => checked(await buildFunction(expr, variables));
 
-export const checked = <R>(func: Fn<R>): Fn<R> => function(...args: number[]): R
-    {
-        if(args.length != func.length)
-            throw new WebAssembly.RuntimeError(`Signature mismatch: passed ${args.length}, expected ${func.length}.`);
-        return func(...args);
-    };
+export const checked = <R>(func: Fn<R>): Fn<R> => function (...args: number[]): R {
+    if (args.length != func.length)
+        throw new WebAssembly.RuntimeError(`Signature mismatch: passed ${args.length}, expected ${func.length}.`);
+    return func(...args);
+};
 
 export { buildFunction, getVariables } from "./compiler";
+
+
+/**
+    1.	Пользователь вызывает parseCompileAndExecute("a + b*2", 3, 4).
+    2.	parseExpr строит AST.
+    3.	getVariables возвращает список переменных ["a","b"].
+    4.	buildFunction создаёт модуль WebAssembly и возвращает JS-функцию.
+    5.	checked проверяет количество аргументов.
+    6.	Функция выполняется с переданными числами, возвращается результат.
+ */
