d35bb56ec0513b90fa932b028aac1f42c70bb3c4
Gleb Gleb <g.estraikh@gmail.com>
2025-10-21 17:53:35 +0700

    add first working version

--- lab07/src/cost.ts
+++ lab07/src/cost.ts
@@ -1,6 +1,22 @@
-import { Expr } from "../../lab04";
+import { Expr, NumConst, Variable, BinaryOp, UnaryMinus } from "../../lab04";
 
-export function cost(e: Expr): number
-{
-    throw "Not implemented";
+export function cost(e: Expr): number {
+    switch (e.type) {
+        case 'const':
+            return 0;
+
+        case 'var':
+            return 1;
+
+        case 'unary':
+            const unaryExpr = e as UnaryMinus;
+            return 1 + cost(unaryExpr.argument);
+
+        case 'binop':
+            const binopExpr = e as BinaryOp;
+            return 1 + cost(binopExpr.left) + cost(binopExpr.right);
+
+        default:
+            throw new Error(`Unknown expression type: ${(e as any).type}`);
+    }
 }
