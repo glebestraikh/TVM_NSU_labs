f6385f68e77cbb0c7d48802ae656a684cd418a36
Gleb Gleb <g.estraikh@gmail.com>
2025-12-01 02:56:12 +0700

    add first working version

--- lab10/src/funnier.ts
+++ lab10/src/funnier.ts
@@ -1,5 +1,41 @@
-import { Module } from 'lab08/src';
-
+import { Module, FunctionDef, Condition, Statement as BaseStatement } from '../../lab08';
 
 export interface AnnotatedModule extends Module {
+    formulas?: FormulaDef[];
+}
+
+export interface AnnotatedFunctionDef extends FunctionDef {
+    precondition?: Predicate;
+    postcondition?: Predicate;
+    invariant?: Predicate;
+}
+
+export interface FormulaDef {
+    type: 'formula';
+    name: string;
+    parameters: { type: "param"; name: string; varType?: 'int' | 'int[]' }[];
+    body: Predicate;
+}
+
+export interface WhileStmtWithInvariant {
+    kind: 'while';
+    condition: Condition;
+    body: Statement;
+    invariant?: Predicate;
+}
+
+export type Statement = BaseStatement | WhileStmtWithInvariant;
+
+export type Predicate = Condition | Quantifier | FormulaRef;
+
+export interface Quantifier {
+    kind: 'forall' | 'exists';
+    variable: { type: "param"; name: string; varType?: 'int' | 'int[]' };
+    predicate: Predicate;
+}
+
+export interface FormulaRef {
+    kind: 'formulaRef';
+    name: string;
+    args: any[];
 }
